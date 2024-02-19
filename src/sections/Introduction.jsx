3f97e6ee971e4acff761106c13f4bd987d78dd8a
WalidAit69 <aitwalid2000@gmail.com>
2024-02-19 12:50:10 +0100

    animations

--- src/sections/Introduction.jsx
+++ src/sections/Introduction.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import handsome from "../assets/profile.jpg";
 import particles from "../assets/particles.svg";
 import longparticle from "../assets/longparticle.svg";
 import "../App.css";
+import { motion, useAnimation, useInView } from "framer-motion"
 
 function Introduction() {
+
+  const divRef = useRef(null);
+
+  const h1controls = useAnimation();
+  const spancontrols = useAnimation();
+
+  const isinview = useInView(divRef);
+
+  useEffect(() => {
+    if (isinview) {
+      h1controls.start({
+        top: "0px",
+      });
+
+      spancontrols.start({
+        bottom: "0px",
+      });
+
+    } else {
+      h1controls.start({
+        top: "170px",
+      });
+
+      spancontrols.start({
+        bottom: "-80px",
+      });
+    }
+
+  }, [h1controls, spancontrols, isinview]);
+
   return (
     <div className='section1'>
       <div className="presentation-section container">
         <div className="left-presentation">
           <div>
-            <h1>Fullstack <span>Developer</span></h1>
-            <h2>I like to craft solid and scalable fullstack products with great user experiences.</h2>
+            <div className='h1title' ref={divRef}>
+              <motion.h1 initial={{ top: "170px" }} animate={h1controls} transition={{ duration: .3, ease: 'easeInOut' }}>Fullstack</motion.h1>
+              <motion.span initial={{ bottom: "-80px" }} animate={spancontrols} transition={{ duration: .3, delay: .3, ease: 'easeInOut' }} className='h1span'>Developer</motion.span>
+            </div>
+            <motion.h2 initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .3, delay: .6, ease: 'easeInOut' }}>I like to craft solid and scalable fullstack products with great user experiences.</motion.h2>
           </div>
 
           <div>
-            <p>Highly skilled at progressive enhancement, design systems & UI Engineering.</p>
-            <p>Proven experience building successful products for clients across several countries.</p>
+            <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .3, delay: .7, ease: 'easeInOut' }}>Highly skilled at progressive enhancement, design systems & UI Engineering.</motion.p>
+            <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: .3, delay: .8, ease: 'easeInOut' }}>Proven experience building successful products for clients across several countries.</motion.p>
           </div>
         </div>
 
         <div className="right-presentation">
           <div className="handsome">
             <figure>
-              <img src={handsome} className='handsome-img' alt="a very handsome man" />
+              <motion.img initial={{ height: 0 }} whileInView={{ height: "100%" }} transition={{ duration: 1, ease: 'easeInOut' }} src={handsome} className='handsome-img' alt="a very handsome man" />
               <img className='particles' src={particles} />
             </figure>
           </div>
@@ -31,7 +65,7 @@ function Introduction() {
       </div>
 
       <img src={longparticle} className='longparticle' alt="" />
-    </div>
+    </div >
   )
 }
 
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import particles1 from "../assets/simpleparticle1.svg";
 import particles2 from "../assets/simpleparticle2.svg";
+import { motion } from "framer-motion";
 
 function Skills() {
     return (
         <div className='section2'>
             <div className='skills container'>
                 <article className="left-skills">
-                    <h1>Design</h1>
-                    <p>I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me (~_^). I'm committed to creating fluent user experiences while staying fashionable.</p>
+                    <motion.h1 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .5, ease: 'easeInOut' }}>Design</motion.h1>
+                    <motion.p initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .5, delay: .2, ease: 'easeInOut' }}>I'm probably not the typical designer positioned behind an Illustrator artboard adjusting pixels, but I design. Immersed in stylesheets tweaking font sizes and contemplating layouts is where you'll find me (~_^). I'm committed to creating fluent user experiences while staying fashionable.</motion.p>
                     <img src={particles2} />
                 </article>
 
                 <article className="right-skills">
                     <div>
-                        <h1>Engineering</h1>
-                        <p>In building JavaScript applications, I'm equipped with just the right tools, and can absolutely function independently of them to deliver fast, resilient solutions optimized for scale — performance and scalabilty are priorities on my radar.</p>
+                        <motion.h1 initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .5, ease: 'easeInOut' }}>Engineering</motion.h1>
+                        <motion.p initial={{ opacity: 0, y: 50 }} whileInView={{ opacity: 1, y: 0 }} transition={{ duration: .5, delay: .2, ease: 'easeInOut' }}>In building JavaScript applications, I'm equipped with just the right tools, and can absolutely function independently of them to deliver fast, resilient solutions optimized for scale — performance and scalabilty are priorities on my radar.</motion.p>
                     </div>
                     <img src={particles1} />
                 </article>
